c6ff5f4af4e5ca98cd9966711bd243803a63d0e4
KruchDmitriy <kruch.dmitriy@gmail.com>
2017-05-10 02:41:28 +0300

    small refactor

--- ui/processing_text.py
+++ ui/processing_text.py
@@ -1,46 +1,45 @@
 # -*- coding: utf-8 -*-
 
-import os
 import re
 import gensim
 import pymorphy2
 import logging
 
-
-logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',\
-    level=logging.INFO)
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',
+                    level=logging.INFO)
 
 model = gensim.models.KeyedVectors.load_word2vec_format('../models/model.bin.gz', binary=True)
 model.init_sims(replace=True)
 
-
 morph = pymorphy2.MorphAnalyzer()
-punct = re.compile('^(.*?)([а-яА-ЯёЁ-]+)(.*?)$')
-capit = re.compile('^[А-Я]+$')
+punctuation_regexp = re.compile('^(.*?)([а-яА-ЯёЁ-]+)(.*?)$')
+capital_regexp = re.compile('^[А-Я]+$')
 
 pth_source = 'books_before/'
 pth_result = 'books_result/'
 
-cotags = {'ADJF':'ADJ', # pymorphy2: word2vec
-'ADJS' : 'ADJ',
-'ADVB' : 'ADV',
-'COMP' : 'ADV',
-'GRND' : 'VERB',
-'INFN' : 'VERB',
-'NOUN' : 'NOUN',
-'PRED' : 'ADV',
-'PRTF' : 'ADJ',
-'PRTS' : 'VERB',
-'VERB' : 'VERB'}
+cotags = {'ADJF': 'ADJ',  # pymorphy2: word2vec
+          'ADJS': 'ADJ',
+          'ADVB': 'ADV',
+          'COMP': 'ADV',
+          'GRND': 'VERB',
+          'INFN': 'VERB',
+          'NOUN': 'NOUN',
+          'PRED': 'ADV',
+          'PRTF': 'ADJ',
+          'PRTS': 'VERB',
+          'VERB': 'VERB'}
 
 capit_letters = [chr(x) for x in range(1040, 1072)] + ['Ё']
+
+
 def search_neighbour(word, pos, gend='masc'):
     word = word.replace('ё', 'е')
     lex = word + '_' + cotags[pos]
     if lex in model:
         neighbs = model.most_similar([lex], topn=20)
-        for nei in neighbs:
-            lex_n, ps_n = nei[0].split('_')
+        for neighbour in neighbs:
+            lex_n, ps_n = neighbour[0].split('_')
             if '::' in lex_n:
                 continue
             if cotags[pos] == ps_n:
@@ -60,6 +59,7 @@ def search_neighbour(word, pos, gend='masc'):
                     return lex_n
     return None
 
+
 def flection(lex_neighb, tags):
     tags = str(tags)
     tags = re.sub(',[AGQSPMa-z-]+? ', ',', tags)
@@ -67,24 +67,25 @@ def flection(lex_neighb, tags):
     tags = re.sub('([A-Z]) (plur|masc|femn|neut|inan)', '\\1,\\2', tags)
     tags = tags.replace("Impe neut", "")
     tags = tags.split(',')
+
     tags_clean = []
-    for t in tags:
-        if t:
-            if ' ' in t:
-                t1, t2 = t.split(' ')
-                t = t2
-            tags_clean.append(t)
+    for tag in tags:
+        if tag and ' ' in tag:
+            _, tag = tag.split(' ')
+            tags_clean.append(tag)
+
     tags = frozenset(tags_clean)
+
     prep_for_gen = morph.parse(lex_neighb)
-    ana_array = []
-    for ana in prep_for_gen:
-        if ana.normal_form == lex_neighb:
-            ana_array.append(ana)
-    for ana in ana_array:
+    info_words = []
+    for info in prep_for_gen:
+        if info.normal_form == lex_neighb:
+            info_words.append(info)
+    for info in info_words:
         try:
-            flect = ana.inflect(tags)
+            flect = info.inflect(tags)
         except:
-            print(tags)
+            print("oops" + str(tags))
             return None
         if flect:
             word_to_replace = flect.word
@@ -92,120 +93,93 @@ def flection(lex_neighb, tags):
     return None
 
 
-def run(s):
+def parse_with_punctuation(before_punct, word, after_punct, was_capital):
+    if was_capital:
+        word = word.capitalize()
+    return before_punct + word + after_punct
 
-    cash_neighb = {}
 
-    i = 0
-    f = []
-    f.append(s)
-    for line in f:
-        new_line = []
-        i += 1
-        line = line.strip()
-        words = line.split(' ')
-        for word in words:
-            struct = punct.findall(word)
-            if struct:
-                struct = struct[0]
-            else:
-                new_line.append(word)
-                continue
-            # print (struct)
-            wordform = struct[1]
-            if wordform:
-                if capit.search(wordform):
-                    new_line.append(word)
-                    continue
+def parse_nominative(word, parse_result, was_capital, lex_neighb, punct):
+    if parse_result.tag.case == 'nomn' and parse_result.tag.number == 'sing':
+        return parse_with_punctuation(punct[0], lex_neighb, punct[2], was_capital)
+
+    word_to_replace = flection(lex_neighb, parse_result.tag)
+    if word_to_replace:
+        return parse_with_punctuation(punct[0], word_to_replace, punct[2], was_capital)
+
+    return word
+
+
+def process_word(word, cash_neighb):
+    struct = punctuation_regexp.findall(word)
+    if struct:
+        struct = struct[0]
+    else:
+        return word
+
+    word_form = struct[1]
+    if word_form:
+        if capital_regexp.search(word_form):
+            return word
+
+        was_capital = word_form[0] in capit_letters
+
+        parse_result = morph.parse(word_form)[0]
+        if 'Name' in parse_result.tag \
+                or 'Patr' in parse_result.tag \
+                or parse_result.normal_form == 'глава':
+            return word
+
+        pos_flag = False
+        for tag in cotags:
+            if tag in parse_result.tag:
+                pos_flag = True
+                lex = parse_result.normal_form
+                pos_tag = parse_result.tag.POS
+
+                if (lex, pos_tag) in cash_neighb:
+                    lex_neighb = cash_neighb[(lex, pos_tag)]
                 else:
-                    if wordform[0] in capit_letters:
-                        capit_flag = 1
+                    if pos_tag == 'NOUN':
+                        gen_tag = parse_result.tag.gender
+                        lex_neighb = search_neighbour(lex, pos_tag, gend=gen_tag)
                     else:
-                        capit_flag = 0
-                parse_result = morph.parse(wordform)[0]
-                if 'Name' in parse_result.tag or 'Patr' in parse_result.tag:
-                    new_line.append(word)
-                    continue
-                if parse_result.normal_form == 'глава':
-                    new_line.append(word)
-                    continue
-                pos_flag = 0
-                for tg in cotags:
-                    if tg in parse_result.tag:
-                        pos_flag = 1
-                        lex = parse_result.normal_form
-                        pos_tag = parse_result.tag.POS
-                        if (lex, pos_tag) in cash_neighb:
-                            lex_neighb = cash_neighb[(lex, pos_tag)]
-                        else:
-                            if pos_tag == 'NOUN':
-                                gen_tag = parse_result.tag.gender
-                                lex_neighb = search_neighbour(lex, pos_tag, gend=gen_tag)
-                            else:
-                                lex_neighb = search_neighbour(lex, pos_tag)
-                            cash_neighb[(lex, pos_tag)] = lex_neighb
-                        if not lex_neighb:
-                            new_line.append(word)
-                            break
-                        else:
-                            if pos_tag == 'NOUN':
-                                if parse_result.tag.case == 'nomn' and parse_result.tag.number == 'sing':
-                                    if capit_flag == 1:
-                                        lex_neighb = lex_neighb.capitalize()
-                                    new_line.append(struct[0] + lex_neighb + struct[2])
-                                else:
-                                    word_to_replace = flection(lex_neighb, parse_result.tag)
-                                    if word_to_replace:
-                                        if capit_flag == 1:
-                                            word_to_replace = word_to_replace.capitalize()
-                                        new_line.append(struct[0] + word_to_replace + struct[2])
-                                    else:
-                                        new_line.append(word)
+                        lex_neighb = search_neighbour(lex, pos_tag)
+                    cash_neighb[(lex, pos_tag)] = lex_neighb
 
-                            elif pos_tag == 'ADJF':
-                                if parse_result.tag.case == 'nomn' and parse_result.tag.number == 'sing':
-                                    if capit_flag == 1:
-                                        lex_neighb = lex_neighb.capitalize()
-                                    new_line.append(struct[0] + lex_neighb + struct[2])
-                                else:
-                                    word_to_replace = flection(lex_neighb, parse_result.tag)
-                                    if word_to_replace:
-                                        if capit_flag == 1:
-                                            word_to_replace = word_to_replace.capitalize()
-                                        new_line.append(struct[0] + word_to_replace + struct[2])
-                                    else:
-                                        new_line.append(word)
+                if not lex_neighb:
+                    return word
 
-                            elif pos_tag == 'INFN':
-                                if capit_flag == 1:
-                                    lex_neighb = lex_neighb.capitalize()
-                                new_line.append(struct[0] + lex_neighb + struct[2])
+                if pos_tag in ['NOUN', 'ADJF']:
+                    return parse_nominative(word, parse_result, was_capital, lex_neighb, struct)
 
-                            elif pos_tag in ['ADVB', 'COMP', 'PRED']:
-                                if capit_flag == 1:
-                                    lex_neighb = lex_neighb.capitalize()
-                                new_line.append(struct[0] + lex_neighb + struct[2])
+                if pos_tag in ['INFN', 'ADVB', 'COMP', 'PRED']:
+                    return parse_with_punctuation(struct[0], lex_neighb, struct[2], was_capital)
 
-                            else:
-                                word_to_replace = flection(lex_neighb, parse_result.tag)
-                                if word_to_replace:
-                                    if capit_flag == 1:
-                                        word_to_replace = word_to_replace.capitalize()
-                                    new_line.append(struct[0] + word_to_replace + struct[2])
-                                else:
-                                    new_line.append(word)
-                        break
-                if pos_flag == 0:
-                    new_line.append(word)
-            else:
-                new_line.append(''.join(struct))
-        line_replace = ' '.join(new_line)
-    return line_replace
+                word_to_replace = flection(lex_neighb, parse_result.tag)
+                if word_to_replace:
+                    return parse_with_punctuation(struct[0], word_to_replace, struct[2], was_capital)
+
+                return word
+        if not pos_flag:
+            return word
+    else:
+        return ''.join(struct)
+
+
+def run(text):
+    cash_neighbours = {}
+    words = text.split(' ')
+    result_text = ""
+    for word in words:
+        result_text += process_word(word, cash_neighbours) + ' '
+    return result_text
 
 
 def main():
     while True:
-        s = str(input())
-        print(run(s))
+        text = str(input())
+        print(run(text))
+
 
 main()
